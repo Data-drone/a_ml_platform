f6de5f23b9827064b31fa06545813a9ab9a3cf9e
Data-drone <bpl.law@gmail.com>
2021-06-25 22:35:53 +1000

    updated the submodules and also updated some env flags to try to make it all work as one stack

--- startup-fullsystem.sh
+++ startup-fullsystem.sh
@@ -5,4 +5,11 @@
 #docker network create ml_platform
 
 ## start the system
-spark_data_eng_template/docker-compose -f sched_docker-compose.yml up --build
+
+
+(cd spark_data_eng_template ; docker-compose -f docker-compose.yml -p datalake --force-recreate --env-file ../.env up)
+
+(cd airflow_template ; docker-compose -f docker-compose.yml -p airflow_scheduler --env-file ../.env up)
+
+(cd feature_store ; docker-compose -f docker-compose.yml --env-file ../.env up)
+# -p datalake --force-recreate --env-file ../.env --build up)
